fbaaf3df8292a1052e719f628d1bcb6d860eb1d6
Julia <ibralenko6724809@gmail.com>
2023-07-18 23:48:30 +0300

    start POST

---
+++ POST/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: ,
+    body:
+    }),
+  headers: {
+    "Content-Type": 'Content-Type': 'application/json; charset=UTF-8'
+  }
+});
