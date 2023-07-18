7c21b76f4c12569e018921ff7db65b170ee55194
Julia <ibralenko6724809@gmail.com>
2023-07-19 00:00:50 +0300

    copy post met.

--- POST/index.js
+++ POST/index.js
@@ -1,12 +1,20 @@
 'use strict'
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: ,
-    body:
-    }),
-  headers: {
-    "Content-Type": 'Content-Type': 'application/json; charset=UTF-8'
-  }
+
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+postData("https://jsonplaceholder.typicode.com/posts",
+{ answer: 42 })
+.then((data) => {
+  console.log(data);
 });
+
