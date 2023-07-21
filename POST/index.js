8eeefb943726727f66f62822ff50cb6cbd3ec734
Julia <ibralenko6724809@gmail.com>
2023-07-21 13:07:13 +0300

    need to add event change (checkInputs)

--- POST/index.js
+++ POST/index.js
@@ -4,21 +4,19 @@ const form = document.forms.postForm;
 const inputTitle = document.getElementById('title-post')
 const inputText = document.getElementById('text-post')
 const posts = document.querySelector('.posts')
+const error = document.querySelector('.error')
+const btn = document.querySelector('.btn')
 
-
-function getTitle ()  {
-  if (inputTitle.value === ''){
-    return alert('Введите название')
-  } return inputTitle.value;
+function checkInputs () {
+  if (inputTitle.value === '' || inputText.value === ''){
+    return document.querySelector('.btn').disabled = true;
+  } else {
+    return document.querySelector('.btn').disabled = false;
+  }
 }
 
-function getText ()  {
-  if (inputText.value === ''){
-    return alert('Введите текст поста')
-  } return inputText.value
-}
-
-function createStructure (title, text){
+checkInputs()
+function createPost (title, text){
   const divPost = document.createElement('div');
   divPost.classList.add('post-container');
   const titlePost = document.createElement('h3');
@@ -26,14 +24,14 @@ function createStructure (title, text){
   titlePost.innerHTML = title;
   const textPost = document.createElement('p');
   textPost.classList.add('text-post')
-  textPost.textContent = text;
+  textPost.innerText = text;
   posts.append(divPost)
   divPost.append(titlePost, textPost)
 }
 
 function piblishPost(){
-  const title = getTitle();
-  const text = getText();
+  const title = inputTitle.value;
+  const text = inputText.value;
 
   fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
@@ -46,7 +44,7 @@ function piblishPost(){
     }
   })
     .then(response => response.json())
-    .then(json => createStructure(json.title, json.text))
+    .then(json => createPost(json.title, json.text))
 }
 
 
