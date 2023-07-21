7ab730f94465a92048af1b8fbabb6eca227dc449
Julia <ibralenko6724809@gmail.com>
2023-07-21 21:01:44 +0300

    done

--- GET/index.js
+++ GET/index.js
@@ -4,14 +4,12 @@ const container = document.querySelector('.container')
 const createPost = (title, body) => {
     const containerPost = document.createElement('div')
     containerPost.classList.add('container-post')
-    const titlePost = document.createElement('h2')
-    titlePost.classList.add('title')
-    titlePost.textContent = `${title}`
-    const textPost = document.createElement('p')
-    textPost.textContent =`${body}`
-    textPost.classList.add('text')
     container.append(containerPost)
-    containerPost.append(titlePost, textPost)
+    containerPost.innerHTML =
+    `
+    <h2 class="title">${title}</h2>
+    <p class="text">${body}</p>
+    `
 }
 
 fetch('https://jsonplaceholder.typicode.com/posts')
