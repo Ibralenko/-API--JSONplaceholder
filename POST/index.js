c033f9914d49e99e4a46a1f8f89247724f51b2e8
Julia <ibralenko6724809@gmail.com>
2023-07-21 12:52:01 +0300

    add js

--- POST/index.js
+++ POST/index.js
@@ -1,17 +1,52 @@
 'use strict'
+
+const form = document.forms.postForm;
+const inputTitle = document.getElementById('title-post')
+const inputText = document.getElementById('text-post')
 const posts = document.querySelector('.posts')
-const postBtn = document.querySelector('.btn')
 
 
+function getTitle ()  {
+  if (inputTitle.value === ''){
+    return alert('Введите название')
+  } return inputTitle.value;
+}
 
+function getText ()  {
+  if (inputText.value === ''){
+    return alert('Введите текст поста')
+  } return inputText.value
+}
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: title,
-    body: text
+function createStructure (title, text){
+  const divPost = document.createElement('div');
+  divPost.classList.add('post-container');
+  const titlePost = document.createElement('h3');
+  titlePost.classList.add('title')
+  titlePost.innerHTML = title;
+  const textPost = document.createElement('p');
+  textPost.classList.add('text-post')
+  textPost.textContent = text;
+  posts.append(divPost)
+  divPost.append(titlePost, textPost)
+}
+
+function piblishPost(){
+  const title = getTitle();
+  const text = getText();
+
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: title,
+      body: text
     }),
-  headers: {
-    'Content-Type': 'application/json; charset=UTF-8'
-  }
-});
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    }
+  })
+    .then(response => response.json())
+    .then(json => createStructure(json.title, json.text))
+}
+
+
